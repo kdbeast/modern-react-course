5379bdfba836a7830f0c0c441a753f1b837d0405
Karan <karanjamwal2004@gmail.com>
2025-08-01 23:30:55 +0530

    Crypto Dash initial setup

---
+++ crypto-dash/src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import { useState } from "react";
+const API_URL =
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+
+function App() {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const res = await fetch(API_URL);
+        if (!res.ok) throw new Error("Failed to fetch data");
+        const data = await res.json();
+        console.log(data);
+        setCoins(data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCoins();
+  }, []);
+
+  return (
+    <>
+      <h1>🚀 Crypto-Dash</h1>
+    </>
+  );
+}
+
+export default App;
