357eb11f739fb299246c7844bc213afa19e5f119
Karan <karanjamwal2004@gmail.com>
2025-08-04 13:11:29 +0530

    react-router setup

--- crypto-dash/src/App.jsx
+++ crypto-dash/src/App.jsx
@@ -1,9 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import CoinCard from "./components/CoinCard";
-import LimitSelector from "./components/LimitSelector";
-import FilterInput from "./components/FilterInput";
-import SortSelector from "./components/SortSelector";
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/home";
 const API_URL = import.meta.env.VITE_API_URL;
 
 function App() {
@@ -12,7 +10,7 @@ function App() {
   const [error, setError] = useState(null);
   const [limit, setLimit] = useState(5);
   const [filter, setFilter] = useState("");
-  const [sort, setSort] = useState("market_cap_desc");
+  const [sortBy, setSortBy] = useState("market_cap_desc");
 
   useEffect(() => {
     const fetchCoins = async () => {
@@ -38,56 +36,25 @@ function App() {
     fetchCoins();
   }, [limit]);
 
-  const filteredCoins = coins
-    .filter((coin) => {
-      return (
-        coin.name.toLowerCase().includes(filter.toLowerCase()) ||
-        coin.symbol.toLowerCase().includes(filter.toLowerCase())
-      );
-    })
-    .slice()
-    .sort((a, b) => {
-      switch (sort) {
-        case "market_cap_desc":
-          return b.market_cap - a.market_cap;
-        case "market_cap_asc":
-          return a.market_cap - b.market_cap;
-        case "price_desc":
-          return b.current_price - a.current_price;
-        case "price_asc":
-          return a.current_price - b.current_price;
-        case "change_desc":
-          return b.price_change_percentage_24h - a.price_change_percentage_24h;
-        case "change_asc":
-          return a.price_change_percentage_24h - b.price_change_percentage_24h;
-        default:
-          return 0;
-      }
-    });
-
   return (
-    <div>
-      <h1>🚀 Crypto-Dash</h1>
-
-      <div className="top-controls">
-        <FilterInput filter={filter} onFilterChange={setFilter} />
-        <SortSelector sort={sort} setSort={setSort} />
-        <LimitSelector limit={limit} setLimit={setLimit} />
-      </div>
-
-      {loading && <p>Loading...</p>}
-      {error && <p className="error">Error: {error?.message}</p>}
-
-      {!loading && !error && (
-        <main className="grid">
-          {filteredCoins.length === 0 ? (
-            <p>No coins found</p>
-          ) : (
-            filteredCoins.map((coin) => <CoinCard key={coin.id} coin={coin} />)
-          )}
-        </main>
-      )}
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <HomePage
+            coins={coins}
+            filter={filter}
+            setFilter={setFilter}
+            limit={limit}
+            setLimit={setLimit}
+            sortBy={sortBy}
+            setSortBy={setSortBy}
+            loading={loading}
+            error={error}
+          />
+        }
+      />
+    </Routes>
   );
 }
 
--- crypto-dash/src/components/SortSelector.jsx
+++ crypto-dash/src/components/SortSelector.jsx
@@ -1,8 +1,8 @@
-const SortSelector = ({ sort, setSort }) => {
+const SortSelector = ({ sortBy, setSortBy }) => {
   return (
     <div className="controls">
       <label htmlFor="limit">Sort By:</label>
-      <select value={sort} onChange={(e) => setSort(e.target.value)}>
+      <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
         <option value="market_cap_desc">Market Cap (High to Low)</option>
         <option value="market_cap_asc">Market Cap (Low to High)</option>
         <option value="price_desc">Price (High to low)</option>
