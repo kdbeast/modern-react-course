6ed3351dea6deec68ba26c1e04af907dba42b21e
Karan <karanjamwal2004@gmail.com>
2025-08-06 14:59:27 +0530

    fetched the products from json server

--- shopping-cart-ui/src/App.jsx
+++ shopping-cart-ui/src/App.jsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+import ProductList from "./components/ProductList";
+
 function App() {
-  return <>Shopping Cart UI</>;
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch("/api/products");
+        if (!res.ok) {
+          throw new Error("Failed to fetch products");
+        }
+        const data = await res.json();
+        setProducts(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold mb-6">🛒 Product Catalog</h1>
+      {loading && <p>Loading...</p>}
+      {error && <p>❌ {error.message}</p>}
+
+      <ProductList products={products} />
+    </div>
+  );
 }
 
 export default App;
